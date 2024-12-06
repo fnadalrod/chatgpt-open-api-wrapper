661c8e639db6a19f0ce56f6ab978e009218f300f
Francisco Nadal <francisconadal1@gmail.com>
2024-12-06 21:02:20 +0100

    [FIX] Removed comments and unused dependencies

--- src/HttpClient/GuzzleHttpClient.php
+++ src/HttpClient/GuzzleHttpClient.php
@@ -30,59 +30,25 @@ class GuzzleHttpClient implements HttpClientInterface
      */
     public function prompt(OpenApiRequest $request): OpenApiResponse {
         try {
-//            $response = $this->client->post(
-//                'https://api.openai.com/v1/chat/completions',
-//                [
-//                    'headers' => [
-//                        'Authorization' => 'Bearer ' . $request->getToken(),
-//                        'Content-Type' => 'application/json',
-//                    ],
-//                    'json' => [
-//                        'model' => $request->getEngine(),
-//                        'messages' => [[
-//                            'role'=> 'user',
-//                            'content'=> $request->getText()
-//                        ]],
-//                        'max_tokens' => $request->getMaxTokens(),
-//                    ],
-//                ]
-//            );
-//
-//            $body = $response->getBody()->getContents();
-            $body = '{
-  "id": "chatcmpl-Ab954JzXnzdOVj17hT1V4ZRXx2kCl",
-  "object": "chat.completion",
-  "created": 1733416406,
-  "model": "gpt-3.5-turbo-0125",
-  "choices": [
-    {
-      "index": 0,
-      "message": {
-        "role": "assistant",
-        "content": "As an AI, I am based on software algorithms and programming, so I am always \"working\" in the sense that I am ready to respond to any queries or tasks given to me. If you are referring to a specific function or task, please",
-        "refusal": null
-      },
-      "logprobs": null,
-      "finish_reason": "length"
-    }
-  ],
-  "usage": {
-    "prompt_tokens": 11,
-    "completion_tokens": 50,
-    "total_tokens": 61,
-    "prompt_tokens_details": {
-      "cached_tokens": 0,
-      "audio_tokens": 0
-    },
-    "completion_tokens_details": {
-      "reasoning_tokens": 0,
-      "audio_tokens": 0,
-      "accepted_prediction_tokens": 0,
-      "rejected_prediction_tokens": 0
-    }
-  },
-  "system_fingerprint": null
-}';
+            $response = $this->client->post(
+                'https://api.openai.com/v1/chat/completions',
+                [
+                    'headers' => [
+                        'Authorization' => 'Bearer ' . $request->getToken(),
+                        'Content-Type' => 'application/json',
+                    ],
+                    'json' => [
+                        'model' => $request->getEngine(),
+                        'messages' => [[
+                            'role'=> 'user',
+                            'content'=> $request->getText()
+                        ]],
+                        'max_tokens' => $request->getMaxTokens(),
+                    ],
+                ]
+            );
+
+            $body = $response->getBody()->getContents();
 
             return OpenApiResponseFactory::build($body);
         } catch (ClientException $clientException) {
